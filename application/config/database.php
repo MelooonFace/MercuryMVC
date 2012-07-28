3ae487052bb8f2c95bcbcc6aa25e9f732208f867
Jacob Marshall <jacob.christian.marshall@gmail.com>
2012-07-29 09:25:31 +1200

    Added database config
    Added the missing file that I had to remove my password from in the
    previous commit.

---
+++ application/config/database.php
@@ -0,0 +1,10 @@
+<?php
+
+	// Start database information file
+	$Config['database'] = array();
+	
+	$Config['database']['host']	= "localhost";
+	$Config['database']['port'] = 3306;
+	$Config['database']['user']	= "root";
+	$Config['database']['pass']	= "";
+	$Config['database']['db']	= "Mercury";
